0b838f21003c1d9314e169393e66a30640378722
rafaelospinacastro <81756302+rafaelospinacastro@users.noreply.github.com>
2025-11-21 15:30:29 -0500

    Agregar archivos de proyecto.

---
+++ PruebaAppinitBackend/Controllers/GamesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PruebaAppinit.Application.DTOs;
+using PruebaAppinit.Application.Services;
+
+namespace Rps.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GamesController : ControllerBase
+{
+    private readonly GameService _service;
+    public GamesController(GameService service) => _service = service;
+
+    [HttpPost("start")]
+    public async Task<IActionResult> Start([FromBody] StartGameRequest req, CancellationToken ct)
+    {
+        var res = await _service.StartGameAsync(req, ct);
+        return CreatedAtAction(nameof(Get), new { id = res.GameId }, res);
+    }
+
+    [HttpPost("play")]
+    public async Task<IActionResult> Play([FromBody] PlayRoundRequest req, CancellationToken ct)
+    {
+        var res = await _service.PlayRoundAsync(req, ct);
+        return Ok(res);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> Get(Guid id, CancellationToken ct)
+    {
+        var res = await _service.GetStatusAsync(id, ct);
+        return Ok(res);
+    }
+}
